704af7eaa2cabc396c133bb84e9d09f9fd6f07f6
steve-caiula <caiulastefano@gmail.com>
2026-03-16 10:03:29 +0100

    Add TWI driver skeleton with ISR structure and buffer variables

---
+++ include/lcd.h
@@ -0,0 +1,21 @@
+#ifndef LCD_H
+#define LCD_H
+
+// TWI STATES
+#define TWI_START           0x08
+#define TWI_REP_START       0x10
+#define TWI_MT_SLA_ACK      0x18  // Address sent, ACK received
+#define TWI_MT_SLA_NACK     0x20  // Address sent, NACK received
+#define TWI_MT_DATA_ACK     0x28  // Data sent, ACK received
+#define TWI_MT_DATA_NACK    0x30  // Data sent, NACK received
+
+// PCF8574 ADDRESS
+#define PCF8574_ADDR        0x27
+
+// LCD CONTROLS
+#define LCD_RS              0x01  // P0
+#define LCD_RW              0x02  // P1
+#define LCD_EN              0x04  // P2
+#define LCD_BL              0x08  // P3 (backlight)
+
+#endif
--- src/lcd.c
+++ src/lcd.c
@@ -1,2 +1,29 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include "lcd.h"
 #include "pin_definitions.h"
+
+volatile uint8_t twi_status;       // Updated by ISR
+volatile uint8_t twi_data;         // Data to transmit
+volatile uint8_t twi_busy;         // 1 = transmission in progress, 0 = idle
+volatile uint8_t twi_buffer[16];   // data buffer to transmit
+volatile uint8_t twi_buffer_len;   // number of bytes to transmit
+volatile uint8_t twi_buffer_index; // current buffer index
+
+ISR(TWI_vect)
+{
+    switch (TWSR & 0xF8) // Status bit mask
+    {
+        case TWI_START: // START sent
+            // Send PCF8574 address
+            break;
+
+        case TWI_MT_SLA_ACK: // Address sent, ACK received
+            // Send data
+            break;
+
+        case TWI_MT_DATA_ACK: // Data sent, ACK received
+            // Send STOP or next data
+            break;
+    }
+}
